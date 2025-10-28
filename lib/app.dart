694bda03f913dd2c1af12bf1d237ff6edde983c4
sudipeb <sudip.koirala@ebpearls.com>
2025-10-28 17:20:53 +0545

    flutter bloc timer app created

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:bloc_timer/timer/timer_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Timer',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
+      home: const TimerPage(),
+    );
+  }
+}
